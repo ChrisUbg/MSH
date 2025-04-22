8bef92533db1f42367d1adbc77ed7548605a5709
ChrisUbg <christof@egger.co>
2025-04-22 13:28:26 +0200

    ss

--- src/MSH.Core/obj/Debug/net8.0/MSH.Core.AssemblyInfo.cs
+++ src/MSH.Core/obj/Debug/net8.0/MSH.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MSH.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5f7ef25133d8ba249ee3e2a56928d7218b78c5b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d127b484fac3ce30ce3dad2d7eaf77f143cc581")]
 [assembly: System.Reflection.AssemblyProductAttribute("MSH.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MSH.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
